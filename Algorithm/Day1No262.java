6a37a6a67ebdd370c8f39d1eb08d5c2812e8aef0
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-11-29 16:12:08 +0700

    no 262

--- Algorithm/Day1No262.java
+++ Algorithm/Day1No262.java
@@ -1,5 +1,25 @@
 package Algorithm;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day1No262 {
 
+    static Map<Character, Integer> characterAppearCount(String inputString) {
+        Map<Character, Integer> countMap = new HashMap<>();
+
+        for (int i = 0; i < inputString.length(); i++) {
+            char tempChar = inputString.charAt(i);
+            if (!countMap.containsKey(tempChar)) {
+                countMap.put(tempChar, 0);
+            }
+            countMap.put(tempChar, countMap.get(tempChar) + 1);
+        }
+        return countMap;
+    }
+
+    public static void main(String[] args) {
+        String a = "aaabbbccccd";
+        System.out.println(characterAppearCount(a));
+    }
 }
