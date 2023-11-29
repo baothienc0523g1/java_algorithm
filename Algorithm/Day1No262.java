573272e2aa356cd411056cfeb93ca9033c8fc5fb
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-11-29 16:22:17 +0700

    no 262

--- Algorithm/Day1No262.java
+++ Algorithm/Day1No262.java
@@ -7,16 +7,18 @@ public class Day1No262 {
 
     /**
      * input: String
-     * count how many times every character appear*/
+     * count how many times every character appear
+     */
     static Map<Character, Integer> characterAppearCount(String inputString) {
         Map<Character, Integer> countMap = new HashMap<>();
 
         for (int i = 0; i < inputString.length(); i++) {
             char tempChar = inputString.charAt(i);
             if (!countMap.containsKey(tempChar)) {
-                countMap.put(tempChar, 0);
+                countMap.put(tempChar, 1);
+            } else {
+                countMap.put(tempChar, countMap.get(tempChar) + 1);
             }
-            countMap.put(tempChar, countMap.get(tempChar) + 1);
         }
         return countMap;
     }
