1df16bfb8e8a61022b8683674133d8b646b65287
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-12-05 08:32:59 +0700

    day 4

--- Algorithm/Day4No267.java
+++ Algorithm/Day4No267.java
@@ -1,6 +1,22 @@
 package Algorithm;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Day4No267 {
 
+    static Map<String, Integer> appearCount(ArrayList<String> inputArray) {
+        Map<String, Integer> result = new HashMap<>();
 
+        for (String temp : inputArray) {
+            if (!result.containsKey(temp)) {
+                result.put(temp, 1);
+            } else {
+                result.put(temp, result.get(temp) + 1);
+            }
+        }
+
+        return result;
+    }
 }
