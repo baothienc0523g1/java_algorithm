70012ed19439f4cfd401b299c22e00ac622eea51
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-12-25 17:16:04 +0700

    ads

--- Algorithm/Day13No286.java
+++ Algorithm/Day13No286.java
@@ -1,8 +1,32 @@
 package Algorithm;
 
 public class Day13No286 {
-    /**
-     * sort and search algorithm
-     */
+    private int id;
+    private String name;
 
+    public Day13No286(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Day13No286{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static void nameChane(Day13No286 day13No286) {
+        day13No286 = new Day13No286(1, "Thang");
+    }
+
+    public static void main(String[] args) {
+        Day13No286 day13No286 = new Day13No286(2, "Thien");
+
+        System.out.println(day13No286.toString());
+        nameChane(day13No286);
+
+        System.out.println(day13No286.toString());
+    }
 }
